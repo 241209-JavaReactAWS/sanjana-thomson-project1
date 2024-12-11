33580338e8e11e8fa6c057736ae4398b7993bbbb
Sanjana Sidgiddi <sansid247700@gmail.com>
2024-12-11 13:50:24 -0500

    Project 1 Intilialized

---
+++ src/main/java/com/revature/ItemManagementCrudApp/ItemManagementCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.revature.ItemManagementCrudApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemManagementCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemManagementCrudAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/ItemManagementCrudApp/ItemManagementCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.ItemManagementCrudApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemManagementCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
